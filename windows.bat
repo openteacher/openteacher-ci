8fe5468370351a3948d15150d7558dbbb04d479d
Marten de Vries <m@rtendevri.es>
2017-06-26 21:05:03 +0200

    Fix Windows packaging

--- windows.bat
+++ windows.bat
@@ -2,8 +2,7 @@ git submodule update --remote
 set PATH=C:\Python36;C:\Python36\Scripts;%PATH%
 
 REM TODO: pyttsx, if fixed, requires pypiwin32
-pip install chardet pyenchant urwid pyenchant Pillow git+https://github.com/pyinstaller/pyinstaller.git@develop
-choco install pyqt5
+pip install pyqt5 chardet pyenchant urwid pyenchant Pillow git+https://github.com/pyinstaller/pyinstaller.git@develop
 
 mkdir output
 cd openteacher
@@ -11,5 +10,4 @@ move moduleFilterer.pyx moduleFilterer.py
 
 REM TODO: tesseract portable + then also re-enable it in pyinstallerInterface
 python openteacher.py -p package-windows-portable ../output/openteacher-portable.zip
-REM FIXME: get wix configured properly
 python openteacher.py -p package-windows-msi ../output/OpenTeacher.msi
